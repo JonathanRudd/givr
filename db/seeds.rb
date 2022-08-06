dabe4946b3cabb833e8c1a1cf1e44fa3cc13c48d
Jonathan Rudd <jonathanrudd3@gmail.com>
2022-08-06 11:51:33 +0900

    v basic user seeds with keanu as generic user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+require 'open-uri'
+
+
+puts "Destroying all users"
+User.destroy_all
+
+puts "creating Keanu and 5 other users"
+
+User.create!(
+  email: "Keanu@gmail.com",
+  password: "123456"
+)
+
+5.times do
+  User.create!(
+    email: Faker::Internet.email,
+    password: "123456"
+  )
+end
+
+# 20.times do
+#   Item.create!(
+#     user_id: user_ids.sample,
+#   )
+# end

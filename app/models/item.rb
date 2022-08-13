a7d2ce839d56650af08ff31f1c203859731ea6f8
Jonathan Rudd <jonathanrudd3@gmail.com>
2022-08-13 10:20:45 +0900

    merged

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,9 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_many_attached :images
-<<<<<<< HEAD
+
   acts_as_taggable_on :tags
-=======
 
   # geocoded_by :address
   # after_validation :geocode, if: :will_save_change_to_address?
->>>>>>> master
 end

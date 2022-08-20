c03fadf6dddce0be9f66af6f3a91cdcdcbc52707
ashish shrestha <ashishrajshrestha1@gmail.com>
2022-08-20 13:17:03 +0900

    Working on msg

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,6 @@
 class MessagesController < ApplicationController
+  def new
+    @pickup = Pickup.find(params[:pickup_id])
+    @message = @pickup.messages.new(user_id: params[:user_id])
+  end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,7 @@
 class Message < ApplicationRecord
   belongs_to :pickup
   belongs_to :user
+  has_many :replies, foreign_key: :user_id, dependent: :destroy
+
   validates :content, presence: true
 end

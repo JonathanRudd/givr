afb5ec27b7398f2ed43078f2e7f1b78db03c44c5
Jonathan Rudd <jonathanrudd3@gmail.com>
2022-08-23 18:51:27 +0900

    progress on url pull but not right yet

--- app/services/imagga.rb
+++ app/services/imagga.rb
@@ -7,7 +7,7 @@ api_secret = "e9fd89b7c25ce2d86e11a8242206c985"
 
 auth = 'Basic ' + Base64.strict_encode64( "#{api_key}:#{api_secret}" ).chomp
 
-image_url = "https://res.cloudinary.com/jh2tc/image/upload/v1660979559/production/yx7iwqmxmp3q22ygl2qftob66nyx.jpg"
+image_url = "http://res.cloudinary.com/jh2tc/image/upload/v1/development/%23%3CActiveStorage::Attachment:0x00007fe77e468f80%3E"
 
 response = RestClient.get "https://api.imagga.com/v2/tags?image_url=#{image_url}", { :Authorization => auth }
 response = JSON.parse(response)

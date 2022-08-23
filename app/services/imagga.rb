60082578a6b0579d59f8ca196a53d763a359e1f1
Jonathan Rudd <jonathanrudd3@gmail.com>
2022-08-23 23:36:53 +0900

    created controller but not sure about implementation - call api after create?

--- app/services/imagga.rb
+++ app/services/imagga.rb
@@ -7,7 +7,7 @@ api_secret = "e9fd89b7c25ce2d86e11a8242206c985"
 
 auth = 'Basic ' + Base64.strict_encode64( "#{api_key}:#{api_secret}" ).chomp
 
-image_url = "http://res.cloudinary.com/jh2tc/image/upload/v1/development/%23%3CActiveStorage::Attachment:0x00007fe77e468f80%3E"
+image_url = "http://res.cloudinary.com/jh2tc/image/upload/v1/development/y9anbymfe45jifp6z0qw89fhhaif"
 
 response = RestClient.get "https://api.imagga.com/v2/tags?image_url=#{image_url}", { :Authorization => auth }
 response = JSON.parse(response)

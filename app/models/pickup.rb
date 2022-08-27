baa832e5a19c84989cee1c6c62efcd2bcfb0b990
January Amanda <january.amanda@gmail.com>
2022-08-27 14:22:02 +0900

    Enum for time is done

--- app/models/pickup.rb
+++ app/models/pickup.rb
@@ -1,7 +1,7 @@
 class Pickup < ApplicationRecord
   belongs_to :user
   belongs_to :item
-  enum time: { AM: 0, PM: 1}
+  enum time: { Before_8_AM: 0, Morning_8_to_12: 1, Afternoon_12_to_4: 2, Evening_4_to_8: 3, After_8_PM: 4 }
   enum status: { pending: 0, accepted: 1, rejected: 2, cancelled: 3}
   has_many :messages, dependent: :destroy
 end

200df84ce0733d7fc412c9c6625d73db95ecf3c9
ashish shrestha <ashishrajshrestha1@gmail.com>
2022-08-30 21:15:54 +0900

    still trying to fix error

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,7 +1,7 @@
-import { Application } from "stimulus"
-import { definitionsFromContext } from "stimulus/webpack-helpers"
 
-const application = Application.start()
-const context = require.context("controllers", true, /_controller\.js$/)
-application.load(definitionsFromContext(context))
+import { Application } from "@hotwired/stimulus"
+import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
 
+window.Stimulus = Application.start()
+const context = require.context("./controllers", true, /_controller\.js$/)
+Stimulus.load(definitionsFromContext(context))

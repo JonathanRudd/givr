b1e98e423f8639b5ee432d9aa0574572f3531c23
mark_houlli <markhoulli@protonmail.com>
2022-09-02 01:22:36 +0900

    added another seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -104,98 +104,102 @@ surf.save!
   itemseed.save!
 end
 
+# Quality item seeds...
+
+file = URI.open('https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/NES-Console-Set.jpg/1200px-NES-Console-Set.jpg')
 item-a = Item.create!(
   user: User.first.id,
   title: "Bicycle",
-  description: "Stylish purple bike that will get you to the top! Framesize 49cmProfessionally checked and serviced by a top-level bike mechanic, incl. 3 months technical guarantee.",
+  description: "Stylish purple bike that will get you to the top! Framesize 49cm. Professionally checked and serviced by a top-level bike mechanic, incl. 3 months technical guarantee.",
   timeframe: "Weekdays morning"
 )
 item-a.tag_list.add("sports equipment")
+f.images.attach(io: file, filename: 'nes.png', content_type: 'image/png')
 item-a.save!
 
-item-b = Item.create!(
-  user: User.first.id,
-  title: "Snowboard",
-  description: "Preowned but in great condition. As used item it's a minor chip and scratches but thes don't compromise the performance. Snowboard w Xylo bindings 138 cm.",
-  timeframe: "Only on weekends morning"
-)
-item-b.tag_list.add("sports equipment")
-item-b.save!
+# item-b = Item.create!(
+#   user: User.first.id,
+#   title: "Snowboard",
+#   description: "Preowned but in great condition. As used item it's a minor chip and scratches but thes don't compromise the performance. Snowboard w Xylo bindings 138 cm.",
+#   timeframe: "Only on weekends morning"
+# )
+# item-b.tag_list.add("sports equipment")
+# item-b.save!
 
-item-c = Item.create!(
-  user: User.first.id,
-  title: "Golf Club",
-  description: "TAYLORMADE R15 Driver, LOFT: 12°, SHAFT: Fujikura Speeder 57 Evolution Graphite Stiff, Right Hand, VERY NICE CONDITION!!! NO SCRATCHES, DENTS, or PAINT CHIPS",
-  timeframe: "Preferred weekdays after 8PM"
-)
-item-c.tag_list.add("sports equipment")
-item-c.save!
+# item-c = Item.create!(
+#   user: User.first.id,
+#   title: "Golf Club",
+#   description: "TAYLORMADE R15 Driver, LOFT: 12°, SHAFT: Fujikura Speeder 57 Evolution Graphite Stiff, Right Hand, VERY NICE CONDITION!!! NO SCRATCHES, DENTS, or PAINT CHIPS",
+#   timeframe: "Preferred weekdays after 8PM"
+# )
+# item-c.tag_list.add("sports equipment")
+# item-c.save!
 
-item-d = Item.create!(
-  user: User.first.id,
-  title: "Lacrosse Shafts",
-  description: "New Brine DV8 women's lacrosse shaft. Lot of two. I have several colors so just let me know which ones you want. Been in storage for awhile but in perfect shape. Thanks for looking.",
-  timeframe: "Mon/Tue/Wed morning only"
-)
-item-d.tag_list.add("sports equipment")
-item-d.save!
+# item-d = Item.create!(
+#   user: User.first.id,
+#   title: "Lacrosse Shafts",
+#   description: "New Brine DV8 women's lacrosse shaft. Lot of two. I have several colors so just let me know which ones you want. Been in storage for awhile but in perfect shape. Thanks for looking.",
+#   timeframe: "Mon/Tue/Wed morning only"
+# )
+# item-d.tag_list.add("sports equipment")
+# item-d.save!
 
-item-e = Item.create!(
-  user: User.first.id,
-  title: "Hockey",
-  description: "Brand New Left Handed Warrior LX Pro | 110 Flex Custom Kink Toe Curve | Ryan O'Reilly - #X53265",
-  timeframe: "Weekdays "
-)
-item-e.tag_list.add("sports equipment")
-item-e.save!
+# item-e = Item.create!(
+#   user: User.first.id,
+#   title: "Hockey",
+#   description: "Brand New Left Handed Warrior LX Pro | 110 Flex Custom Kink Toe Curve | Ryan O'Reilly - #X53265",
+#   timeframe: "Weekdays "
+# )
+# item-e.tag_list.add("sports equipment")
+# item-e.save!
 
 file = URI.open('https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/NES-Console-Set.jpg/1200px-NES-Console-Set.jpg')
 f = Item.new(
-  user: User.find(1),
+  user: User.find{1},
   title: "Nintendo set",
   description: "This is a classically styled vintage, possibly antique piece. Solid wood throughout with elegantly turned legs.",
   timeframe: "Weekday evenings"
 )
-f.tag_list.add("furniture, antiques")
+f.tag_list.add("furniture", "antiques")
 f.images.attach(io: file, filename: 'nes.png', content_type: 'image/png')
 f.save!
 
 
-item-g = Item.create!(
-  user: User.first.id,
-  title: "Old leather couch",
-  description: "Vintage condition and a perfect statement in any room. Sad to be giving it away, will be happy to see it go to a cool new home!!",
-  timeframe: "Weekends"
-)
-item-g.tag_list.add("furniture, retro")
-item-g.save!
+# item-g = Item.create!(
+#   user: User.first.id,
+#   title: "Old leather couch",
+#   description: "Vintage condition and a perfect statement in any room. Sad to be giving it away, will be happy to see it go to a cool new home!!",
+#   timeframe: "Weekends"
+# )
+# item-g.tag_list.add("furniture, retro")
+# item-g.save!
 
-item-h = Item.create!(
-  user: User.first.id,
-  title: "Oak wardrobe",
-  description: "Solid pine wardrobe - excellent condition and very well made with a beautiful beeswax finish. Width 90cm, height 187cm",
-  timeframe: "Monday evenings"
-)
-item-h.tag_list.add("furniture")
-item-h.save!
+# item-h = Item.create!(
+#   user: User.first.id,
+#   title: "Oak wardrobe",
+#   description: "Solid pine wardrobe - excellent condition and very well made with a beautiful beeswax finish. Width 90cm, height 187cm",
+#   timeframe: "Monday evenings"
+# )
+# item-h.tag_list.add("furniture")
+# item-h.save!
 
-item-i = Item.create!(
-  user: User.first.id,
-  title: "Cool coat stand",
-  description: "Beautiful wooden coat stand in excellent condition. Height 192cm, width 55cm.",
-  timeframe: "Weekday mornings"
-)
-item-i.tag_list.add("furniture, fashion")
-item-i.save!
+# item-i = Item.create!(
+#   user: User.first.id,
+#   title: "Cool coat stand",
+#   description: "Beautiful wooden coat stand in excellent condition. Height 192cm, width 55cm.",
+#   timeframe: "Weekday mornings"
+# )
+# item-i.tag_list.add("furniture, fashion")
+# item-i.save!
 
-item-j = Item.create!(
-  user: User.first.id,
-  title: "Solid Pine shabby-chic bookcase. It's in good clean condition. Was an unfinished project, but Will make an easy painting project for someone.",
-  description: "Works fine, looks good",
-  timeframe: "Any day after 6pm"
-)
-item-j.tag_list.add("furniture")
-item-j.save!
+# item-j = Item.create!(
+#   user: User.first.id,
+#   title: "Solid Pine shabby-chic bookcase. It's in good clean condition. Was an unfinished project, but Will make an easy painting project for someone.",
+#   description: "Works fine, looks good",
+#   timeframe: "Any day after 6pm"
+# )
+# item-j.tag_list.add("furniture")
+# item-j.save!
 
 # file = URI.open('https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/NES-Console-Set.jpg/1200px-NES-Console-Set.jpg')
 # item = Item.new()

37ca5b6c110122a947100e3f0555cb8a9a2c00ef
January Amanda <january.amanda@gmail.com>
2022-09-01 23:17:18 +0900

    star rating average done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,6 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-//= require jquery.raty
-//= require ratyrate
-
-require("jquery.raty").start()
-require("ratyrate").start()
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
